b7d2bc7db3e0f31fdd639c3da3a340aeb3bab114
sumit007ind <sharmaraj81808@gmail.com>
2025-10-10 23:38:36 +0530

    /

--- Helpmate/src/Pages/HomePage.jsx
+++ Helpmate/src/Pages/HomePage.jsx
@@ -267,12 +267,7 @@ const HomePage = () => {
                   >
                     Login
                   </Link>
-                  <Link
-                    to="/signup"
-                    className="block px-3 py-2 text-white bg-red-600 rounded-lg mx-3 text-center hover:bg-red-700"
-                  >
-                    Sign Up
-                  </Link>
+                
                 </div>
               </div>
             </div>
